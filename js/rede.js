436c5c60daec727bdd97a48103464ddf69da1bcd
MIIGgames <98579374+MIIGgames@users.noreply.github.com>
2023-04-23 15:34:50 -0300

    up

--- js/rede.js
+++ js/rede.js
@@ -1,8 +1,13 @@
 const div1 = document.getElementById('pagina1');
-const div2 = document.getElementById('pagina1');
+const div2 = document.getElementById('pagina2');
 
 
 div1.addEventListener('click', function() {
+  const url = this.getAttribute('data-url');
+  window.location.href = url;
+});
+
+div2.addEventListener('click', function() {
   const url = this.getAttribute('data-url');
   window.location.href = url;
 });

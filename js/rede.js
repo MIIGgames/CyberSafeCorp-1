505d972c35de3ce740e26a15aeb5fa4a3ce2d328
MIIGgames <98579374+MIIGgames@users.noreply.github.com>
2023-05-21 00:47:43 -0300

    up

--- js/rede.js
+++ js/rede.js
@@ -1,6 +1,5 @@
 const div1 = document.getElementById('pagina1');
 const div2 = document.getElementById('pagina2');
-const div3 = document.getElementById('pagina3');
 
 
 div1.addEventListener('click', function() {
@@ -13,13 +12,12 @@ div2.addEventListener('click', function() {
   window.location.href = url;
 });
 
-div3.addEventListener('click', function() {
-  const url = this.getAttribute('data-url');
-  window.location.href = url;
+const curso = document.getElementById('pagina-curso');
+curso.addEventListener('click', function() {
+  window.location.href = 'https://curso.cybersafecorp.com.br/';
 });
 
 const conheca = document.getElementById('conheca-mais');
-
 conheca.addEventListener('click', function() {
   window.location.href = 'https://boletim.cybersafecorp.com.br';
 });
